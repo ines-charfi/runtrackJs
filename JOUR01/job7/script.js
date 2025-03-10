309124d4e0478f3c0efcf68474ea4003b8e9deba
Ines Jaffel <ines.charfi@laplateforme.io>
2025-03-10 15:27:58 +0100

    job7 terminé

--- JOUR01/job7/script.js
+++ JOUR01/job7/script.js
@@ -1,3 +1,13 @@
-function JourTravaille(){
-    let date = new Date();
+function JourTravaille(jour, mois, annee) {
+    let date = " ";
+    const joursfériés = ["01-01", "01-05", "08-05", "14-07", "15-08", "01-11", "11-11", "25-12"];
+    for (let i = 0; i < joursfériés.length; i++) {
+    if (jour === 1 && mois === 1 || jour === 1 && mois === 5 || jour === 8 && mois === 5 || jour === 14 && mois === 7 || jour === 15 && mois === 8 || jour === 1 && mois === 11 || jour === 11 && mois === 11 || jour === 25 && mois === 12)
+         {
+        console.log("Non, c'est un jour férié");
+    }
+    else {
+        console.log("Oui, c'est un jour travaillé");
+    }
+}
 }
