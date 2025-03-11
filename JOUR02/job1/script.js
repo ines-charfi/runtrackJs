a40eb6c43114f37cbc82e02d875040f5f7dfc001
Ines Jaffel <ines.charfi@laplateforme.io>
2025-03-11 14:34:38 +0100

    job1 terminé

---
+++ JOUR02/job1/script.js
@@ -0,0 +1,7 @@
+function citation() {
+    var citation = "La vie a beaucoup plus d'imagination que nous";
+    document.getElementById("citation").textContent = citation;
+   
+    return citation;
+}
+citation(); // Appel de la fonction
