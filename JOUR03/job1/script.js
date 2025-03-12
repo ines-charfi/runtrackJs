8fa07c2ff42d3f8196a4c073518fce380abb3abd
Ines Jaffel <ines.charfi@laplateforme.io>
2025-03-12 14:56:46 +0100

    job1 terminé

---
+++ JOUR03/job1/script.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+    // Fonction pour afficher le texte
+    $("#afficher").click(function () {
+    $("#citation").show(); // Cache le texte
+});
+
+// Fonction pour cacher le texte
+$("#cacher").click(function () {
+    $("#citation").hide(); // Efface le texte
+});
+});
