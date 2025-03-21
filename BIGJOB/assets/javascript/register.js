ad248b0927d3b6872d731ccdbd43b46a0fcf8488
Ines Jaffel <ines.charfi@laplateforme.io>
2025-03-21 10:18:07 +0100

    fichiers javascript ajoutés

---
+++ BIGJOB/assets/javascript/register.js
@@ -0,0 +1,20 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    // Vérifier si les mots de passe correspondent
+    if (password !== confirmPassword) {
+        alert('Les mots de passe ne correspondent pas.');
+        return;
+    }
+
+    // Simuler une inscription
+    if (email.endsWith('@laplateforme.io')) {
+        alert('Inscription réussie!');
+        window.location.href = 'login.html';
+    } else {
+        alert('Seuls les membres de La Plateforme_ peuvent s\'inscrire.');
+    }
+});
