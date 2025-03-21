34f6f0e32a7da089048c749240818183f58c80a9
Ines Jaffel <ines.charfi@laplateforme.io>
2025-03-21 10:48:27 +0100

    register.js modifié

--- BIGJOB/assets/javascript/register.js
+++ BIGJOB/assets/javascript/register.js
@@ -1,20 +1,25 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    console.log(email, password, confirmPassword);
 
     // Vérifier si les mots de passe correspondent
     if (password !== confirmPassword) {
-        alert('Les mots de passe ne correspondent pas.');
-        return;
+      alert("Les mots de passe ne correspondent pas.");
+      return;
     }
 
     // Simuler une inscription
-    if (email.endsWith('@laplateforme.io')) {
-        alert('Inscription réussie!');
-        window.location.href = 'login.html';
+    if (email.endsWith("@laplateforme.io")) {
+      alert("Inscription réussie!");
+      window.location.href = "login.html";
     } else {
-        alert('Seuls les membres de La Plateforme_ peuvent s\'inscrire.');
+      alert("Seuls les membres de La Plateforme_ peuvent s'inscrire.");
     }
-});
+  });
